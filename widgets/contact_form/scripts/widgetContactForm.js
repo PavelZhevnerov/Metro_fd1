29ad05940958b88b133e8d00431be34b9f3ed3f5
Sergey Olshanskiy <sergey.olshanskiy@live.com>
2016-05-07 00:52:19 +0300

    Added interactive to widget Contact Form

--- widgets/contact_form/scripts/widgetContactForm.js
+++ widgets/contact_form/scripts/widgetContactForm.js
@@ -1,2 +1,18 @@
 (function ($) {
+    var emailRegEx = /([A-Z|a-z|0-9](\.|_){0,1})+[A-Z|a-z|0-9]\@([A-Z|a-z|0-9])+((\.){0,1}[A-Z|a-z|0-9]){2}\.[a-z]{2,3}$/;
+    $(document).ready(function () {
+        $('.input__addButton').on('click', function () {
+            var email = prompt('Please, enter email.', '');
+
+            if (emailRegEx.test(email)){
+                $('.input__valueCollection').append(
+                    $('<div>')
+                        .addClass('input__valueCollectionItem')
+                        .text(email)
+                );
+            } else {
+                alert('"' + email + '" is not email.')
+            }
+        });
+    });
 })(jQuery);
