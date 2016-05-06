031f921a168e084e610ef7d77b7fb0cb1794e5b5
Anton Rymasheuski <supok@tut.by>
2016-05-07 00:03:06 +0300

    js to ribbons

---
+++ widgets/navigation/scripts/navigation.js
@@ -0,0 +1,8 @@
+			function ribbonsLeft() {
+			var elem = document.getElementById('content__ribbons');
+					elem.style.left = '0px';
+			}
+			function ribbonsRight() {
+			var elem = document.getElementById('content__ribbons');
+					elem.style.left = '223px';
+			}
